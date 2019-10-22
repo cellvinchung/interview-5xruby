b6d20cd1927324c63c1f6ffa3483e37b29c1bbfb
鍾劭方（Cellvin Chung） <cellvinchung@gmail.com>
2019-10-22 15:42:47 +0800

    修正travis.yml postgres、補上mission_test feature

--- test/models/mission_test.rb
+++ test/models/mission_test.rb
@@ -4,4 +4,23 @@ class MissionTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "可新增自己的任務" do 
+  end
+  test "使用者登入後，只能看見自己建立的任務" do 
+  end
+  test "可設定任務的開始及結束時間" do 
+  end
+  test "可設定任務的優先順序（高、中、低）" do 
+  end
+  test "可設定任務目前的狀態（待處理、進行中、已完成）" do 
+  end
+  test "可依狀態篩選任務" do 
+  end
+  test "可以任務的標題、內容進行搜尋" do 
+  end
+  test "可為任務加上分類標籤" do 
+  end
+  test "任務列表，並可依優先順序、開始時間及結束時間等進行排序" do 
+  end
 end
